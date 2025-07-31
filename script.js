650006995f629ececee195685e473d41e40a1e22
Joel Bravo <44070165+jbravo-747@users.noreply.github.com>
2025-07-31 23:02:06 +0000

    con iframe

--- script.js
+++ script.js
@@ -42,14 +42,13 @@ function mostrarDetallesPais(pais) {
       <strong>Arancel previamente aplicado o amenazado:</strong> ${info.arancel_prev}<br>
       <strong>Nuevo arancel anunciado:</strong> ${info.arancel_nuevo}<br>
       <strong>Participación en importaciones de EE.UU.:</strong> ${info.participacion}
-      <br><br> 
-      <div style="position: relative; width: 100%; height: 0; padding-top: 112.4451%;
-        padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
-        border-radius: 8px; will-change: transform;">
+      <br><br>
+      <div style="position: relative; width: 100%; height: 0; padding-top: 56.2225%; padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden; border-radius: 8px; will-change: transform;">
         <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
-        src="https://www.canva.com/design/DAGuxMqtXow/uJeCd-UaH0JTn2-0_oVzeA/watch?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+          src="https://www.canva.com/design/DAGuxMqtXow/uJeCd-UaH0JTn2-0_oVzeA/watch?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
         </iframe>
-      </div> `;
+      </div>
+    `;
   } else {
     document.getElementById("country-details").innerHTML = "Información no disponible.";
   }
